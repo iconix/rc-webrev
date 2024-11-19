113f2f8b4c8a0db8ed59f54e3b54269a29a8aee1
iconix <narhodes1@gmail.com>
2024-11-18 23:00:52 -0500

    add rehype-external-links and rehype-autolink-headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
-import svelte from "@astrojs/svelte";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +17,47 @@ export default defineConfig({
   }),
   site: 'https://ohnadj.pages.dev/',
 
-  integrations: [svelte()]
+  integrations: [svelte()],
+
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'prepend',
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: { className: ['heading-link'] },
+            children: [
+              {
+                type: 'element',
+                tagName: 'img',
+                properties: { src: '/link.svg' },
+                children: [],
+              },
+            ],
+          },
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          content: {
+            type: 'element',
+            tagName: 'img',
+            properties: {
+              src: '/external-link.svg',
+              alt: 'External link icon',
+            },
+            children: [],
+          },
+          contentProperties: { className: ['external-link-icon'] },
+          target: '_blank',
+          rel: 'nofollow noopener noreferrer'
+        }
+      ]
+    ]
+  },
 });
